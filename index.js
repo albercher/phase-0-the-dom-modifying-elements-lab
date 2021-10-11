b060ca84bf30e93f6048ae993e23bdff6aae03bf
Abbigale Bercher <abbeybercher@gmail.com>
2021-10-11 12:13:38 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+victory.innerHTML = "<h1>Abbey is the champion</h1>";
